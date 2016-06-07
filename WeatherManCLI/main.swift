6b9004a47f5536eee0453b26701ef98f8342b857
Eric Dejonckheere <eric@aya.io>
2016-06-07 23:21:01 +0200

    Initial Commit

---
+++ WeatherManCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WeatherManCLI
+//
+//  Created by ERIC DEJONCKHEERE on 07/06/2016.
+//  Copyright © 2016 AYA.io. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
